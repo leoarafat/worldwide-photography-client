0091101d75ffd7db451440a440ff9b9c7745d315
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-11-07 20:27:02 +0600

    create components and added navbar

---
+++ src/components/Routes/Router.js
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main/Main";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: '/', element: <Main/>,children: [
+            {
+                path: '/', element: <Home/>
+            },
+        ]
+    }
+])
