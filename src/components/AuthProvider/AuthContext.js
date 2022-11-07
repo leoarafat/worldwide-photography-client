9e6afc3fb9c21e3944f0d3e70d6ba38cd7647b04
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-11-07 21:05:12 +0600

    added favicon and crerate context page

--- src/components/AuthProvider/AuthContext.js
+++ src/components/AuthProvider/AuthContext.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { createContext, useState } from "react";
+import { app } from "../../firebase";
+import { getAuth } from "firebase/auth";
+const auth = getAuth(app);
+export const userContext = createContext();
 
-const AuthContext = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AuthContext = ({ children }) => {
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const userInfo = {
+    user,
+  };
+  return (
+    <userContext.Provider value={userInfo}>{children}</userContext.Provider>
+  );
 };
 
 export default AuthContext;
