ffcf43abb151f5fcaf9a6c0bd3bbfa2be8c89177
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-11-09 10:15:00 +0600

    added add service

--- src/components/pages/AddService/AddService.js
+++ src/components/pages/AddService/AddService.js
@@ -1,81 +1,143 @@
-import React from 'react';
+import React from "react";
 
 const AddService = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const handleSubmit = e =>{
+    const form = e.target;
+    const serviceName = form.serviceName.value;
+    const title = form.title.value;
+    const img = form.photoURL.value;
+    const details = form.details.value;
+    const camera = form.camera.value;
+    const location = form.location.value;
+    const services = {
+      Service_name: serviceName,
+      title,
+      img,
+      details,
+      camera,
+      location
 
-  }
+    };
+    fetch("http://localhost:5000/addService", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(services),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          alert("review successful");
+          form.reset();
+        }
+      });
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Please Review!</h1>
-          <p className="py-6">
-            If you like my photography please leave a review. Thank you.
-          </p>
-        </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Your name"
-                className="input input-bordered"
-                name="name"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="Your email"
-                className="input input-bordered"
-                name="email"
-                
-                required
-                readOnly
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Photo</span>
-              </label>
-              <input
-                type="text"
-                placeholder="PhotoURL"
-                className="input input-bordered"
-                name="photoURL"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Feedback</span>
-              </label>
-              <textarea
-                className="textarea textarea-bordered h-24"
-                placeholder="Leave a feedback"
-                name="feedback"
-                required
-              ></textarea>
-              <label className="label"></label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Submit</button>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Add service</h1>
+            <p className="py-6">If you want then you can add service.</p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Service Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Service Name"
+                  className="input input-bordered"
+                  name="serviceName"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Title"
+                  className="input input-bordered"
+                  name="title"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Camera</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Camera"
+                  className="input input-bordered"
+                  name="camera"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Location</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Location"
+                  className="input input-bordered"
+                  name="location"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Price"
+                  className="input input-bordered"
+                  name="price"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="PhotoURL"
+                  className="input input-bordered"
+                  name="photoURL"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Description</span>
+                </label>
+                <textarea
+                  className="textarea textarea-bordered h-24"
+                  placeholder="Description"
+                  name="details"
+                  required
+                ></textarea>
+                <label className="label"></label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Add Service</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </form>
+    </form>
   );
 };
 
